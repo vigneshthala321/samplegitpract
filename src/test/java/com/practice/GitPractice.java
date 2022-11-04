a03f30ff6d358a342e90ff0cb5063155e655e2ad
vigneshthala321 <vigneshthala321@gmail.com>
2022-11-04 20:51:48 +0530

    first commit

---
+++ src/test/java/com/practice/GitPractice.java
@@ -0,0 +1,9 @@
+package com.practice;
+
+public class GitPractice {
+public static void main(String[] args) {
+	System.out.println("vishnu");
+	System.out.println("sophars");
+	System.out.println("raja");
+}
+}
